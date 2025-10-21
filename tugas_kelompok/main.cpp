1e9edb523582c6018597a8489606b700c4efef51
Putri Liansari <putrilianasari28@gmail.com>
2025-10-21 10:18:20 +0700

    mengubah isi file

--- tugas_kelompok/main.cpp
+++ tugas_kelompok/main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include<fstream>
 using namespace std;
-ofstream filesiswa("data.txt",ios::app);
+fstream filesiswa("data.txt",ios::app);
 struct nilai{
     float Mtk;
     float BIn;
@@ -14,47 +14,66 @@ struct siswa{
     string jurusan;
     nilai Nilai; // struct in struct
 };
-void tambahDataSiswa(siswa data,int n);//menambahkan data pada file siswa.txt
+//tes
+//void tambahDataSiswa(siswa data,int n);//menambahkan data pada file siswa.txt
 /*void tampilDataSiswa();//membaca data dari file siswa.txt dan ditampilkan
 void cariSiswa();// berdasarkan NISN
 float nilaiAkhir();// nilai akhir = 40%matematika+30%IPA+20%B indonesia +20%B Inggris
 void ranking(); // menampilkan data siswa berdasarkan peringkatnya*/
 
-void tambahdatasiswa(siswa data,int n){
+void tambahdatasiswa(siswa data[],int n){
     if (filesiswa.is_open())
     {
         cout << "Menambahkan data siswa ke file data.txt" << endl;
         for (int i = 0; i < n; i++)
         {
+            cout << "input siswa ke "<<i+1<<endl;
             cout << "Masukkan nama siswa: ";
-            cin >> data.nama;
+            cin >> data[i].nama;
             cout << "Masukkan NISN siswa: ";
-            cin >> data.NISN;
+            cin >> data[i].NISN;
             cout << "Masukkan jurusan siswa: ";
-            cin >> data.jurusan;
+            cin >> data[i].jurusan;
             cout << "Masukkan nilai Matematika: ";
-            cin >> data.Nilai.Mtk;
+            cin >> data[i].Nilai.Mtk;
             cout << "Masukkan nilai Bahasa Indonesia: ";
-            cin >> data.Nilai.BIn;
+            cin >> data[i].Nilai.BIn;
             cout << "Masukkan nilai Bahasa Inggris: ";
-            cin >> data.Nilai.Big;
+            cin >> data[i].Nilai.Big;
             cout << "Masukkan nilai IPA: ";
-            cin >> data.Nilai.IPA;
+            cin >> data[i].Nilai.IPA;
+            cout <<endl;
 
-            filesiswa << data.nama << "," << data.NISN << "," << data.jurusan << "," 
-                      << data.Nilai.Mtk << "," << data.Nilai.BIn << "," 
-                      << data.Nilai.Big << "," << data.Nilai.IPA << endl;
+            filesiswa << data[i].nama << endl << data[i].NISN << endl << data[i].jurusan << endl 
+                      << data[i].Nilai.Mtk << endl << data[i].Nilai.BIn << endl 
+                      << data[i].Nilai.Big << endl << data[i].Nilai.IPA << endl;
         } 
+        filesiswa.close();
     }
     
 }
-
+void tampilDataSiswa(siswa data[],int n){
+    //ifstream file("data.txt");
+    string baris;
+    if (!filesiswa.is_open()){
+        cout << "Gagal membuka file!" << endl;
+    }
+    else{
+        while (getline(filesiswa,baris))
+        {
+            cout << baris;
+        }
+        
+        filesiswa.close();
+    }
+}
 int main()
 {
-    siswa data;
     int n;
     cout << "input jumlah siswa : ";
     cin >>n;
-    tambahDataSiswa(data,n); 
+    siswa data[n];
+    tambahdatasiswa(data,n);
+    tampilDataSiswa(data,n);
      
 }
